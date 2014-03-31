d4c011be6d9b124755e38ab1dbd8c6f2823be63d
Sam Fries <samuelbfries@gmail.com>
2014-03-31 16:46:50 -0700

    Added an async model class for subclassing

---
+++ asyncmodel.py
@@ -0,0 +1,39 @@
+from asynclist import AsyncItem, AsyncList
+
+
+class AsyncModel(object):
+    """
+        Map to a remote object, load attributes as needed
+    """
+
+    def __init__(self, id):
+        self.id = id
+        self.__async_attributes__ = {}
+
+    def loader_for_attribute(self, attribute):
+        return lambda x: x
+
+    def map_attributes(self, attrs):
+        for key in attrs:
+            value = attrs[key]
+            loader = loader_for_attribute(key)
+            try:
+                #Iterable
+                mapped = AsyncList(value, loader)
+            except TypeError:
+                #Plain value
+                mapped = AsyncItem(value, loader)
+
+            self.__async_attributes__[key] = mapped
+
+    def __getattr__(self, name):
+        if name in self.__async_attributes__:
+            try:
+                #Plain value
+                return self.__async_attributes__[name].get()
+            except AttributeError:
+                #Iterable
+                return self.__async_attributes__[name]
+        else:
+            raise AttributeError("'%s' object has no attribute '%s'" %
+                                 (type(self), name))
